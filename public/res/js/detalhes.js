577d98eecf14752194f02ebe9a729347cae9503b
Elvis D'Andrea <elvis@gravi.com.br>
2016-01-25 13:16:21 -0200

    favourites events

--- public/res/js/detalhes.js
+++ public/res/js/detalhes.js
@@ -7,3 +7,36 @@ if (GMaps.mapLatitude != undefined && GMaps.mapLatitude != '') {
     GMaps.add_circle(GMaps.mapLatitude, GMaps.mapLongitude);
 }
 
+$(document).on('submit', '#contato-imovel', function(e){
+
+    e.preventDefault();
+    $.ajax({
+        url: $(this).attr('action'),
+        type: 'post',
+        data: $(this).serializeArray(),
+        success: function(r) {
+            $('#contato-imovel').find('.contato-resposta').html(r).show();
+            $('#contato-imovel').find('.contato-resposta').trigger('click');
+            $('#contato-imovel')[0].reset();
+            setTimeout( function() {
+                $.fancybox.close();
+            },3000);
+            return false;
+        }
+    });
+    return false;
+});
+
+$(document).on('click', '#add-fav', function(e){
+    e.preventDefault();
+    $.ajax({
+        url: $(this).attr('href'),
+        type: 'get',
+        success: function(r) {
+            $('#fav-content').fancybox().trigger('click');
+            return false;
+        }
+    });
+    return false;
+});
+
